2176983652798cca990313e4e17874ba335959d9
MansoorMuhammad <muhammadokashag@gmail.com>
2025-05-09 13:28:25 -0700

    sumOfTwoNumber

---
+++ sumofTwoNo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout<<"Enter 1st no :"<<endl;
+    cin>>a;
+    cout<<"Enter 2nd no :"<<endl;
+    cin>>b;
+
+    c = a+b;
+    cout<<"The result is = "<<c<<endl;
+
+}
