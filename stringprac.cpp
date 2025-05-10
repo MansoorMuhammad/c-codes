49e7db5045c7e3de5cc6520e62a2380e1955369f
MansoorMuhammad <muhammadokashag@gmail.com>
2025-05-10 02:31:50 -0700

    HI THERE

---
+++ stringprac.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){ 
+ string string_name="Mansoor Muhammd";
+ cout<<string_name<<endl;   
+ string str("Hi There");
+ cout<<str;
+}
